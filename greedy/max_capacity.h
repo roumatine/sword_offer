de83899c8b3b7298881d668ffa95a96146bcfcb7
roumatine <987226594@qq.com>
2025-04-19 11:11:06 +0800

    finish greedy

--- greedy/max_capacity.h
+++ greedy/max_capacity.h
@@ -1,16 +1,21 @@
 #include "..\head.h"
 
-int max_capacity(vector<int> &ht) {
-    int i = 0;
-    int j = ht.size() - 1;
+/* 最大容量：贪心 */
+int maxCapacity(vector<int> &ht) {
+    // 初始化 i, j，使其分列数组两端
+    int i = 0, j = ht.size() - 1;
+    // 初始最大容量为 0
     int res = 0;
+    // 循环贪心选择，直至两板相遇
     while (i < j) {
+        // 更新最大容量
         int cap = min(ht[i], ht[j]) * (j - i);
         res = max(res, cap);
+        // 向内移动短板
         if (ht[i] < ht[j]) {
-            i ++;
+            i++;
         } else {
-            j --;
+            j--;
         }
     }
     return res;
