0ab73a45fae2ec7072122e8b7428430a95545c93
roumatine <987226594@qq.com>
2025-05-13 12:27:23 +0800

    add greed

---
+++ selected300/greed/1-944.h
@@ -0,0 +1,19 @@
+#include "..\head.h"
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int len1 = strs.size();
+        int len2 = strs[0].size();
+        int res = 0;
+        for (int i = 0; i < len2; ++i) {
+            for (int j = 1; i < len1; ++j) {
+                if (strs[j - 1][i] > strs[j][i]) {
+                    res++;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
